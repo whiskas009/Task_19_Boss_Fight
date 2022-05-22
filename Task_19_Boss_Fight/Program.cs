54b0161f797b4f2ac4c829478c27dfd12f96e50a
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-22 17:21:45 +0300

    #2

--- Task_19_Boss_Fight/Program.cs
+++ Task_19_Boss_Fight/Program.cs
@@ -12,11 +12,19 @@ namespace Task_19_Boss_Fight
         {
             int userHealth = 100;
             int userEnergy = 100;
+
+            int maxHealth = 100;
+            int maxEnergy = 100;
+
             int healthRecovery = 10;
             int energyRecovery = 10;
-
+                 
             int userUsualyAttak = 10;
             int userStrongAttak = 30;
+
+            int increasedDamage = 10;
+            int decreaseHealthRecovery = 20;
+            int statusLexir = 0;
 
             int energyUsualtAttak = 30;
             int energyStrongtAttak = 80;
@@ -31,7 +39,8 @@ namespace Task_19_Boss_Fight
 
                 Console.WriteLine($"Выберите действие: \n1. Выполнить обычную аттаку ({userUsualyAttak} урона и {energyUsualtAttak} энергии)");
                 Console.WriteLine($"2. Выполнить сильную аттаку ({userStrongAttak} урона и {energyStrongtAttak} энергии)");
-                Console.WriteLine($"3. Спрятаться за укрытие и восстановить {energyRecovery} энергии\n");
+                Console.WriteLine($"3. Спрятаться за укрытие и восстановить {energyRecovery} энергии");
+                Console.WriteLine($"4. Выпить лексир повышающий урон (навсегда повышает урон на 10, но уменьшает и здоровья на 20)\n");
                 int userСhoice = int.Parse(Console.ReadLine());
 
                 switch (userСhoice)
@@ -61,15 +70,32 @@ namespace Task_19_Boss_Fight
                         break;
 
                     case 3:
-                        if (userHealth != 100)
+                        if (userHealth != maxHealth)
                         { 
                             userHealth += healthRecovery;
                             userEnergy += energyRecovery;
                         }
-                        else if (userEnergy != 100)
+                        else if (userEnergy != maxEnergy)
                             userEnergy += energyRecovery;
                         else
                             Console.WriteLine("У Вас уже полная энергия и здоровье");
+                        break;
+
+                    case 4:
+                        if (statusLexir == 0)
+                        {
+                            maxHealth -= decreaseHealthRecovery;
+                            userHealth -= decreaseHealthRecovery;
+                            userUsualyAttak += increasedDamage;
+                            userStrongAttak += increasedDamage;
+                            statusLexir = 1;
+                            Console.WriteLine("Лексир применён!");
+                        }
+                        else 
+                            Console.WriteLine("Лексир уже применён!");
+                        break;
+                    
+                    default: Console.WriteLine("Такого варианта нет!\n");
                         break;
                 }
                 
