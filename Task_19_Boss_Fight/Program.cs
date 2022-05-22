5c3e502d8b0f8eb9e4b4fc8114c1cea13c4d72fd
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-22 16:25:41 +0300

    #1

---
+++ Task_19_Boss_Fight/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_19_Boss_Fight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userHealth = 100;
+            int userEnergy = 100;
+            int healthRecovery = 10;
+            int energyRecovery = 10;
+
+            int userUsualyAttak = 10;
+            int userStrongAttak = 30;
+
+            int energyUsualtAttak = 30;
+            int energyStrongtAttak = 80;
+            
+            int enemyHealth = 100;
+            int enemyAttak = 50;
+
+            while (userHealth > 0 && enemyHealth > 0)
+            {
+                Console.WriteLine($"Ваше здоровье: {userHealth} \nВаша энергия: {userEnergy}\n");
+                Console.WriteLine($"Здоровье противника: {enemyHealth} \n");
+
+                Console.WriteLine($"Выберите действие: \n1. Выполнить обычную аттаку ({userUsualyAttak} урона и {energyUsualtAttak} энергии)");
+                Console.WriteLine($"2. Выполнить сильную аттаку ({userStrongAttak} урона и {energyStrongtAttak} энергии)");
+                Console.WriteLine($"3. Спрятаться за укрытие и восстановить {energyRecovery} энергии\n");
+                int userСhoice = int.Parse(Console.ReadLine());
+
+                switch (userСhoice)
+                {
+                    case 1:
+                        if (userEnergy >= energyUsualtAttak)
+                        {
+                            enemyHealth -= userUsualyAttak;
+                            userEnergy -= energyUsualtAttak;
+                            userHealth -= enemyAttak;
+                            Console.WriteLine($"Враг наносит Вам {enemyAttak} урона");
+                        }
+                        else
+                            Console.WriteLine("У Вас недостаточно энергии для данного действия: \n");
+                        break;
+
+                    case 2:
+                        if (userEnergy >= energyStrongtAttak)
+                        {
+                            enemyHealth -= userStrongAttak;
+                            userEnergy -= energyStrongtAttak;
+                            userHealth -= enemyAttak;
+                            Console.WriteLine($"Враг наносит Вам {enemyAttak} урона");
+                        }
+                        else
+                            Console.WriteLine("У Вас недостаточно энергии для данного действия: \n");
+                        break;
+
+                    case 3:
+                        if (userHealth != 100)
+                        { 
+                            userHealth += healthRecovery;
+                            userEnergy += energyRecovery;
+                        }
+                        else if (userEnergy != 100)
+                            userEnergy += energyRecovery;
+                        else
+                            Console.WriteLine("У Вас уже полная энергия и здоровье");
+                        break;
+                }
+                
+            }
+
+            if (userHealth <= 0)
+                Console.WriteLine("Вы погибли");
+            else
+                Console.WriteLine("Противник повержен!");
+        }
+    }
+}
---
+++ Task_19_Boss_Fight/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Task_19_Boss_Fight")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task_19_Boss_Fight")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a603feab-6d58-4dd5-a1d4-a8a546a296ac")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
